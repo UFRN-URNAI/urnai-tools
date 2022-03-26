9cbd01d884421ae75a58826608d4551066cf2d60
Álvaro Ferreira Pires de Paiva <alvarofepipa@gmail.com>
2022-03-25 22:06:40 -0300

    chore(lint): Applies linter to tests folder

--- tests/dql_tf_lambda/main.py
+++ tests/dql_tf_lambda/main.py
@@ -1,10 +1,6 @@
-import gym
-from gym import wrappers
-import numpy as np
-
-from helper import *
 from DeepTDLambdaLearner import DeepTDLambdaLearner
-
+import gym
+from helper import package_state, tweak_reward
 
 name_of_gym = 'FrozenLake-v0'
 episodes = 1000
@@ -17,31 +13,32 @@ agent = DeepTDLambdaLearner(n_actions=n_actions, n_states=n_states)
 
 # Iterate the game
 for e in range(episodes):
-	state = env.reset()
-	state = package_state(state)
+    state = env.reset()
+    state = package_state(state)
 
-	total_reward = 0
-	done = False
-	while not done:
-		action, greedy = agent.get_e_greedy_action(state)
-		next_state, reward, done, _ = env.step(action)
-		# env.render()
-		
-		next_state = package_state(next_state)
-	
-		# Tweaking the reward to help the agent learn faster
-		tweaked_reward = tweak_reward(reward, done)
-		
-		agent.learn(state, action, next_state, tweaked_reward, greedy)
-		
-		state = next_state
-		total_reward += tweaked_reward
-		
-		if done:
-			if reward == 1:
-				print("episode: {}/{}, score: {:.2f} and goal has been found!".format(e, episodes, total_reward))
-			else:
-				print("episode: {}/{}, score: {:.2f}".format(e, episodes, total_reward))
-			break
-	
-	agent.reset_e_trace()
+    total_reward = 0
+    done = False
+    while not done:
+        action, greedy = agent.get_e_greedy_action(state)
+        next_state, reward, done, _ = env.step(action)
+        # env.render()
+
+        next_state = package_state(next_state)
+
+        # Tweaking the reward to help the agent learn faster
+        tweaked_reward = tweak_reward(reward, done)
+
+        agent.learn(state, action, next_state, tweaked_reward, greedy)
+
+        state = next_state
+        total_reward += tweaked_reward
+
+        if done:
+            if reward == 1:
+                print('episode: {}/{}, score: {:.2f} and goal has been found!'.format(e, episodes,
+                                                                                      total_reward))
+            else:
+                print('episode: {}/{}, score: {:.2f}'.format(e, episodes, total_reward))
+            break
+
+    agent.reset_e_trace()
--- tests/integration_tests/test_cartpole.py
+++ tests/integration_tests/test_cartpole.py
@@ -1,6 +1,6 @@
 from urnai.trainers.filetrainer import FileTrainer
 
 def test_solve_cartpole():
-    trainer = FileTrainer("solve_cartpole_v1.json")
+    trainer = FileTrainer('solve_cartpole_v1.json')
     trainer.start_training()
     assert trainer.logger.ep_avg_rewards[-1] >= 500
