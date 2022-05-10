f4a614ed4e465b6846a424f01b5e0ef14018bea6
marcocspc <marcocspc@hotmail.com>
2022-05-10 13:22:30 -0300

    fix: Formatting code to satisfy lint complaints

--- urnai/runner/commands/get.py
+++ urnai/runner/commands/get.py
@@ -1,94 +1,86 @@
 from urnai.runner.base.command import Command
-from urnai.utils.reporter import Reporter as rp
 
 
 class GetCommand(Command):
-
     def __init__(self, top_parser, subparser):
         super().__init__(top_parser, subparser)
         self.command = 'get'
         self.help = 'Subcommand used to get info about Urnai parts.'
-        self.flags = [
-            {
-                'command': '--environments',
-                'help': 'List available environments.',
-                'action': 'store_true',
-            },
-            {
-                'command': '--environment-params',
-                'help': 'Show parameter info about the passed ENVIRONMENT.',
-                'type': str, 'metavar': 'ENVIRONMENT', 
-                'action': 'store',
-            },
-            {
-                'command': '--agents',
-                'help': 'List available agents.',
-                'action': 'store_true',
-            },
-            {
-                'command': '--agent-params',
-                'help': 'Show parameter info about the passed AGENT.',
-                'type': str, 'metavar': 'AGENT', 
-                'action': 'store',
-            },
-            {
-                'command': '--action-wrappers',
-                'help': 'List available action-wrappers.',
-                'action': 'store_true',
-            },
-            {
-                'command': '--action-wrapper-params',
-                'help': 'Show parameter info about the passed ACTION_WRAPPER.',
-                'type': str, 'metavar': 'ACTION_WRAPPER', 
-                'action': 'store',
-            },
-            {
-                'command': '--state-builders',
-                'help': 'List available state-builders.',
-                'action': 'store_true',
-            },
-            {
-                'command': '--state-builder-params',
-                'help': 'Show parameter info about the passed STATE_BUILDER.',
-                'type': str, 'metavar': 'STATE_BUILDER', 
-                'action': 'store',
-            },
-            {
-                'command': '--rewards',
-                'help': 'List available state-builders.',
-                'action': 'store_true',
-            },
-            {
-                'command': '--reward-params',
-                'help': 'Show parameter info about the passed REWARD.',
-                'type': str, 'metavar': 'REWARD', 
-                'action': 'store',
-            },
-            {
-                'command': '--models',
-                'help': 'List available models.',
-                'action': 'store_true',
-            },
-            {
-                'command': '--model-params',
-                'help': 'Show parameter info about the passed MODEL.',
-                'type': str, 'metavar': 'MODEL', 
-                'action': 'store',
-            },
-            {
-                'command': '--default-training-file',
-                'help': 'Generate a base training file in YAML format, redirect the output to a file to save or use --save FILE_NAME',
-                'action' : 'store_true'
-            },
-            {
-                'command': '--save',
-                'help': 'File name to save output to.',
-                'type': str, 'metavar': 'FILE_NAME', 
-                'action': 'store',
-            },
-        ]
+        self.flags = [{'command': '--environments',
+                       'help': 'List available environments.',
+                       'action': 'store_true',
+                       },
+                      {'command': '--environment-params',
+                       'help': 'Show parameter info about the passed ENVIRONMENT.',
+                       'type': str,
+                       'metavar': 'ENVIRONMENT',
+                       'action': 'store',
+                       },
+                      {'command': '--agents',
+                       'help': 'List available agents.',
+                       'action': 'store_true',
+                       },
+                      {'command': '--agent-params',
+                       'help': 'Show parameter info about the passed AGENT.',
+                       'type': str,
+                       'metavar': 'AGENT',
+                       'action': 'store',
+                       },
+                      {'command': '--action-wrappers',
+                       'help': 'List available action-wrappers.',
+                       'action': 'store_true',
+                       },
+                      {'command': '--action-wrapper-params',
+                       'help': 'Show parameter info about the passed ACTION_WRAPPER.',
+                       'type': str,
+                       'metavar': 'ACTION_WRAPPER',
+                       'action': 'store',
+                       },
+                      {'command': '--state-builders',
+                       'help': 'List available state-builders.',
+                       'action': 'store_true',
+                       },
+                      {'command': '--state-builder-params',
+                       'help': 'Show parameter info about the passed STATE_BUILDER.',
+                       'type': str,
+                       'metavar': 'STATE_BUILDER',
+                       'action': 'store',
+                       },
+                      {'command': '--rewards',
+                       'help': 'List available state-builders.',
+                       'action': 'store_true',
+                       },
+                      {'command': '--reward-params',
+                       'help': 'Show parameter info about the passed REWARD.',
+                       'type': str,
+                       'metavar': 'REWARD',
+                       'action': 'store',
+                       },
+                      {'command': '--models',
+                       'help': 'List available models.',
+                       'action': 'store_true',
+                       },
+                      {'command': '--model-params',
+                       'help': 'Show parameter info about the passed MODEL.',
+                       'type': str,
+                       'metavar': 'MODEL',
+                       'action': 'store',
+                       },
+                      {'command': '--default-training-file',
+                       'help': 'Generate a base training file in YAML format, ' +
+                       'redirect the output to a file to save or use --save FILE_NAME',
+                       'action': 'store_true',
+                       },
+                      {'command': '--save',
+                       'help': 'File name to save output to.',
+                       'type': str,
+                       'metavar': 'FILE_NAME',
+                       'action': 'store',
+                       },
+                      ]
 
         self.add_subcommand()
 
     def run(self, args):
-        #TODO code for defined flags
+        # TODO code for defined flags
+        pass
--- urnai/utils/module_specialist.py
+++ urnai/utils/module_specialist.py
@@ -1,8 +1,8 @@
 import ast
 import importlib
+import inspect
 import os
 import pkgutil
-import inspect
 
 from .error import ClassNotFoundError, EnvironmentNotSupportedError
 
@@ -13,15 +13,26 @@ def get_class_import_path(pkg_str, classname):
         mod = importlib.import_module(pkg_str)
         for module_info in pkgutil.iter_modules(mod.__path__):
             if module_info.ispkg:
-                module_path += get_class_import_path(pkg_str + '.' + module_info.name, classname)
+                module_path += get_class_import_path(
+                    pkg_str + '.' + module_info.name, classname,
+                )
             else:
-                file_path = module_info.module_finder.path + os.path.sep + module_info.name + '.py'
+                file_path = (
+                    module_info.module_finder.path
+                    + os.path.sep
+                    + module_info.name
+                    + '.py'
+                )
                 with open(file_path, 'r') as input_file:
                     node = ast.parse(input_file.read())
-                    classes = [n for n in node.body if isinstance(n, ast.ClassDef)]
+                    classes = [
+                        n for n in node.body if isinstance(
+                            n, ast.ClassDef)]
                     for class_node in classes:
                         if class_node.name == classname:
-                            module_path += '.'.join([pkg_str, module_info.name, classname])
+                            module_path += '.'.join(
+                                [pkg_str, module_info.name, classname],
+                            )
     except ModuleNotFoundError:
         pass
     except NameError:
@@ -35,7 +46,9 @@ def get_cls(pkg, classname):
     if cls_str_full != '':
         cls_str_full = cls_str_full.split('.')
     else:
-        raise ClassNotFoundError('{} was not found in {}'.format(classname, pkg))
+        raise ClassNotFoundError(
+            '{} was not found in {}'.format(
+                classname, pkg))
 
     cls_str = cls_str_full.pop()
     pkg_str = cls_str_full
@@ -45,19 +58,29 @@ def get_cls(pkg, classname):
 
     return cls
 
-def get_classes_recursively(pkg_path, ignore = []):
+
+def get_classes_recursively(pkg_path, ignore=[]):
     cls_list = []
 
     try:
         mod = importlib.import_module(pkg_path)
         for module_info in pkgutil.iter_modules(mod.__path__):
             if module_info.ispkg:
-                cls_list += get_classes_recursively(pkg_path + '.' + module_info.name, ignore)
+                cls_list += get_classes_recursively(
+                    pkg_path + '.' + module_info.name, ignore,
+                )
             else:
-                file_path = module_info.module_finder.path + os.path.sep + module_info.name + '.py'
+                file_path = (
+                    module_info.module_finder.path
+                    + os.path.sep
+                    + module_info.name
+                    + '.py'
+                )
                 with open(file_path, 'r') as input_file:
                     node = ast.parse(input_file.read())
-                    cls_list += [n.name for n in node.body if isinstance(n, ast.ClassDef)]
+                    cls_list += [
+                        n.name for n in node.body if isinstance(
+                            n, ast.ClassDef)]
 
     except ModuleNotFoundError:
         pass
@@ -72,33 +95,34 @@ def get_classes_recursively(pkg_path, ignore = []):
 
     return cls_list
 
+
 def get_class_parameters(pkg_path, classname):
     try:
         cls = get_cls(pkg_path, classname)
-    except ModuleNotFoundError as mnfe:
-        if pkg_path == "urnai.envs":
-            raise EnvironmentNotSupportedError("{} returned a ModuleNotFoundError, is {} installed correctly?".format(classname, classname))
+    except ModuleNotFoundError:
+        if pkg_path == 'urnai.envs':
+            raise EnvironmentNotSupportedError(
+                '{} returned a ModuleNotFoundError, is {} installed correctly?'.format(
+                    classname, classname))
         else:
             raise
 
-    if cls is not None: 
+    if cls is not None:
         params = inspect.getargspec(cls.__init__)
         names = params.args
         defaults = params.defaults
 
-        default_names = names[(len(names) - len(defaults)) :]
-        names = names[0 : (len(names) - len(defaults)) ]
+        default_names = names[(len(names) - len(defaults)):]
+        names = names[0: (len(names) - len(defaults))]
         names.pop(0)
 
         param_data = {
-                "params_without_defaults" : names, 
-                "params_with_deaults" : []
-                }
+            'params_without_defaults': names,
+            'params_with_deaults': []}
 
         for param, default_value in zip(default_names, defaults):
-            param_data["params_with_deaults"].append({
-                "param" : param,
-                "default_value" : default_value
-                })
+            param_data['params_with_deaults'].append(
+                {'param': param, 'default_value': default_value},
+            )
 
         return param_data
