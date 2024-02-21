b4ad96a502e301677a48b093806c29bfe6039a9e
CinquilCinquil <106356391+CinquilCinquil@users.noreply.github.com>
2024-02-21 15:22:56 -0300

    chore(issue-67): Create base classes for Action
    #67

---
+++ urnai/actions/action_base.py
@@ -0,0 +1,7 @@
+from abc import ABC, abstractmethod
+
+
+class ActionBase(ABC):
+
+	#def can_build_structure(...):
+		# logic...
---
+++ urnai/actions/chain_of_actions_base.py
@@ -0,0 +1,7 @@
+from abc import ABC, abstractmethod
+
+
+class ChainOfActionsBase(ABC): 
+
+	#def can_gather_resources(...):
+		# logic...
