ff7f643f74c0363ea418ef76d6452cfb57d0bf64
CinquilCinquil <106356391+CinquilCinquil@users.noreply.github.com>
2024-02-22 16:49:08 -0300

    feat(issue-67): Added methods for base classes for Action
    #67

--- urnai/actions/action_base.py
+++ urnai/actions/action_base.py
@@ -1,7 +1,25 @@
 from abc import ABC, abstractmethod
 
+from urnai.utils.returns import ActionIndex, List
+
 
 class ActionBase(ABC):
+		
+	@abstractmethod
+	def get_action(self, action_idx: ActionIndex, obs):
+		"""
+		Receives an action index as a parameter and returns the corresponding action from the 
+		available actions. This method should return an action
+		that can be used by the environment's step method.
+		"""
+		...
+	
+	@abstractmethod
+	def clear_queue(self):
+		...
+	
+	@abstractmethod
+	def is_queue_empty(self) -> bool:
+		...
+	
 
-	#def can_build_structure(...):
-		# logic...
--- urnai/actions/chain_of_actions_base.py
+++ urnai/actions/chain_of_actions_base.py
@@ -1,7 +1,23 @@
 from abc import ABC, abstractmethod
 
+from urnai.utils.returns import ActionIndex, List
+
 
 class ChainOfActionsBase(ABC): 
 
-	#def can_gather_resources(...):
-		# logic...
+	@abstractmethod
+	def get_actions(self) -> List[ActionIndex]:
+		"""Returns all the actions that the agent can choose from."""
+		...
+	
+	@abstractmethod
+	def get_excluded_actions(self, obs) -> List[ActionIndex]:
+		"""Returns a subset of actions that can't be chosen by the agent."""
+		...
+	
+	def get_named_actions(self) -> List[str]:
+		"""Returns the names of all the actions that the agent can choose from."""
+		return None
+
+	def get_action_space_dim(self):
+		return len(self.get_actions())
