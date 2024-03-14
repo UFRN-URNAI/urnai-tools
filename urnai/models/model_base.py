88748f7f78b132c897cbbfe73b17372c42f4a51b
CinquilCinquil <106356391+CinquilCinquil@users.noreply.github.com>
2024-03-14 06:55:11 -0300

    refactor(): Adjusted to reviewer's suggestions
    #80

--- urnai/models/model_base.py
+++ urnai/models/model_base.py
@@ -1,20 +1,24 @@
 from abc import ABC, abstractmethod
+from typing import Dict
 
 from urnai.utils.returns import ActionIndex
 
 
 class ModelBase(ABC):
 
-	learning_data = {}
+	learning_data: Dict
+	
+	def __init__(self):
+		self.learning_data = {}
 	
 	@abstractmethod
-	def learn(self, s, a, r, s_):
+	def learn(self, current_state, action, reward, next_state):
 		"""Learning strategy"""
 		...
 	
 	@abstractmethod
 	def predict(self, state) -> ActionIndex:
-		"""Returns the best action for this give state"""
+		"""Returns the best action for this given state"""
 		...
 	
 	@abstractmethod
