87a79ee92c64cf862e958147c109f81d8221f9d7
CinquilCinquil <106356391+CinquilCinquil@users.noreply.github.com>
2024-03-14 07:13:29 -0300

    test(issue-75): Added unit tests for ModelBase
    #80

---
+++ tests/units/models/test_model_base.py
@@ -0,0 +1,29 @@
+import unittest
+from abc import ABCMeta
+
+from urnai.models.model_base import ModelBase
+
+
+class TestModelBase(unittest.TestCase):
+
+	def test_abstract_methods(self):
+		ModelBase.__abstractmethods__ = set()
+
+		class FakeModel(ModelBase):
+			def __init__(self):
+				super().__init__()
+
+		fake_model = FakeModel()
+		learn_return = fake_model.learn("current_state",
+			"action", "reward", "next_state")
+		predict_return = fake_model.predict("state")
+		save_return = fake_model.save()
+		load_return = fake_model.load()
+		learning_dict = fake_model.learning_dict
+
+		assert isinstance(ModelBase, ABCMeta)
+		assert learn_return is None
+		assert predict_return is None
+		assert save_return is None
+		assert load_return is None
+		assert learning_dict is None
