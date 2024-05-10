be18c1dbeebac4504e69601690613014d0caf9c1
CinquilCinquil <106356391+CinquilCinquil@users.noreply.github.com>
2024-05-10 11:34:38 -0300

    refactor: Adjusted to some of reviewer's suggestions

--- urnai/base/savable.py
+++ urnai/base/savable.py
@@ -15,7 +15,7 @@ class Savable:
         self.threaded_saving = threaded_saving
         self.attr_block_list = []
 
-    def get_default_save_stamp(self):
+    def _get_default_save_stamp(self):
         """
         This method returns the default
         file name that should be used while
@@ -23,7 +23,7 @@ class Savable:
         """
         return self.__class__.__name__ + '_'
 
-    def save_pickle(self, persist_path):
+    def _save_pickle(self, persist_path):
         """
         This method saves our instance
         using pickle.
@@ -40,42 +40,32 @@ class Savable:
         """
         path = self.get_full_persistance_path(persist_path)
 		
-		# creating necessary directories for the file
         os.makedirs(os.path.dirname(path), exist_ok=True)
 		
         with open(path, 'wb') as pickle_out:
-            pickle.dump(self.get_pickleable_dict(), pickle_out)
+            pickle.dump(self._get_pickleable_dict(), pickle_out)
 
-    def save_extra(self, persist_path):
+    def _save_extra(self, persist_path):
         """
         This method should be implemented when
         some extra persistence is to be saved.
         """
         pass
 
-    def load_pickle(self, persist_path):
+    def _load_pickle(self, persist_path):
         """
         This method loads a list instance
         saved by pickle.
         """
-        # Check if pickle file exists
         pickle_path = self.get_full_persistance_path(persist_path)
         exists_pickle = os.path.isfile(pickle_path)
         
-        # If yes, load it
         if exists_pickle and os.path.getsize(pickle_path) > 0:
             with open(pickle_path, 'rb') as pickle_in:
                 pickle_dict = pickle.load(pickle_in)
-                self.restore_attributes(pickle_dict)
-                '''
-                rp.report(
-                    '**************************************** \n Loaded pickle for '
-                    + self.get_default_save_stamp()
-                    + ' loaded. \n****************************************',
-                2)
-				'''
+                self._restore_attributes(pickle_dict)
 
-    def load_extra(self, persist_path):
+    def _load_extra(self, persist_path):
         """
         This method should be implemented when
         some extra persistence is to be loaded.
@@ -84,54 +74,42 @@ class Savable:
 
     def get_full_persistance_path(self, persist_path):
         """This method returns the default persistance path."""
-        return persist_path + os.path.sep + self.get_default_save_stamp()
+        return persist_path + os.path.sep + self._get_default_save_stamp()
 
     def save(self, savepath):
         """
         This method saves pickle objects
         and extra stuff needed
         """
-        #rp.report('Saving {} object...'.format
-		#(self.__class__.__name__), verbosity_lvl=1)
-        #start_time = time.time()
         
         if not self.threaded_saving:
-            self.save_pickle(savepath)
+            self._save_pickle(savepath)
         else:
-            self.threaded_pickle_save(savepath)
+            self._threaded_pickle_save(savepath)
 
-        self.save_extra(savepath)
-        
-        #end_time = time.time()
-        '''
-        rp.report('It took {} seconds to save {} object!'
-		.format(end_time - start_time, self.__class__.__name__),
-        verbosity_lvl=2)
-        '''
+        self._save_extra(savepath)
 
-    def threaded_pickle_save(self, savepath):
+    def _threaded_pickle_save(self, savepath):
         """
         This method saves pickleable
         stuff in a separate thread
         """
-        #rp.report('THREADED Saving {} object...'
-		#.format(self.__class__.__name__), verbosity_lvl=1)
 		
-        p = Process(target=self.save_pickle, args=(savepath,))
-        p.start()
+        new_process = Process(target=self._save_pickle, args=(savepath,))
+        new_process.start()
 
     def load(self, loadpath):
         """
         This method loads pickle objects
         and extra stuff needed
         """
-        self.load_pickle(loadpath)
-        self.load_extra(loadpath)
+        self._load_pickle(loadpath)
+        self._load_extra(loadpath)
 
-    def get_pickleable_attributes(self):
+    def _get_pickleable_attributes(self):
         """
         This method returns a list of pickeable attributes.
-		If you wish to blocklist one particular pickleable attribute, put it
+		If you wish to block one particular pickleable attribute, put it
         in self.attr_block_list as a string.
         """
         if not hasattr(self, 'attr_block_list') or self.attr_block_list is None:
@@ -154,43 +132,38 @@ class Savable:
             except pickle.PicklingError:
                 continue
             
-            except TypeError as te:
-                if "can't pickle" in str(te):
-                    continue
-                if 'cannot pickle' in str(te):
-                    continue
-                else:
+            except TypeError as type_error:
+                if (not "can't pickle" in str(type_error)) or (not 'cannot pickle' in str(type_error)):
                     raise
+                continue
             
-            except NotImplementedError as nie:
-                if (str(nie) == 
+            except NotImplementedError as notimpl_error:
+                if (str(notimpl_error) != 
                 'numpy() is only available when eager execution is enabled.'):
-                    continue
-                else:
                     raise
+                continue
             
-            except AttributeError as ae:
-                if ("Can't pickle" in str(ae) or 
+            except AttributeError as attr_error:
+                if ("Can't pickle" in str(attr_error) or 
                 "object has no attribute '__getstate__'"):
                     continue
             
-            except ValueError as ve:
-                if 'ctypes objects' in str(ve):
-                    continue
-                else:
+            except ValueError as value_error:
+                if not 'ctypes objects' in str(value_error):
                     raise
+                continue
 
         return pickleable_list
 
-    def get_pickleable_dict(self):
+    def _get_pickleable_dict(self):
         pickleable_attr_dict = {}
 
-        for attr in self.get_pickleable_attributes():
+        for attr in self._get_pickleable_attributes():
             pickleable_attr_dict[attr] = getattr(self, attr)
 
         return pickleable_attr_dict
 
-    def restore_attributes(self, dict_to_restore):
+    def _restore_attributes(self, dict_to_restore):
         for key in dict_to_restore:
             if key not in self.attr_block_list and key != 'attr_block_list':
                 setattr(self, key, dict_to_restore[key])
