84ad0efbafbf06a800086ede047a5de7019fa550
CinquilCinquil <106356391+CinquilCinquil@users.noreply.github.com>
2024-06-07 14:38:07 -0300

    test(issue-68): Add unit tests for ActionSpaceBase
    Also modified ActionBase's tests to test action name.

---
+++ tests/units/actions/test_action_space_base.py
@@ -0,0 +1,36 @@
+import unittest
+from abc import ABCMeta
+from unittest.mock import MagicMock
+
+from urnai.actions.action_space_base import ActionSpaceBase
+
+
+class FakeActionSpace(ActionSpaceBase):
+    ActionSpaceBase.__abstractmethods__ = set()
+    ...
+
+class TestActionSpaceBase(unittest.TestCase):
+
+    def test_abstract_methods(self):
+        fake_action_space = FakeActionSpace()
+        is_action_done_return = fake_action_space.is_action_done()
+        reset_return = fake_action_space.reset()
+        get_actions_return = fake_action_space.get_actions()
+        get_excluded_actions_return = fake_action_space.get_excluded_actions("obs")
+        get_actions_return = fake_action_space.get_action(0, "obs")
+
+        assert isinstance(ActionSpaceBase, ABCMeta)
+        assert is_action_done_return is None
+        assert reset_return is None
+        assert get_actions_return is None
+        assert get_excluded_actions_return is None
+
+    def test_get_named_actions(self):
+        fake_action_space = FakeActionSpace()
+        fake_action_space.get_named_actions = MagicMock(return_value=[])
+        self.assertEqual(fake_action_space.get_named_actions(), [])
+
+    def test_size(self):
+        fake_action_space = FakeActionSpace()
+        fake_action_space.get_actions = MagicMock(return_value=[])
+        self.assertEqual(fake_action_space.size, 0)
