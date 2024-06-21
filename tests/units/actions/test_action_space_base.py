8aa748a3684111eff14ba86f4ab8ccf97d002903
CinquilCinquil <106356391+CinquilCinquil@users.noreply.github.com>
2024-06-21 10:12:18 -0300

    refactor(issue-68): Tests now in "given when then" format
    #89

--- tests/units/actions/test_action_space_base.py
+++ tests/units/actions/test_action_space_base.py
@@ -12,13 +12,18 @@ class FakeActionSpace(ActionSpaceBase):
 class TestActionSpaceBase(unittest.TestCase):
 
     def test_abstract_methods(self):
+
+        # GIVEN
         fake_action_space = FakeActionSpace()
+
+        # WHEN
         is_action_done_return = fake_action_space.is_action_done()
         reset_return = fake_action_space.reset()
         get_actions_return = fake_action_space.get_actions()
         get_excluded_actions_return = fake_action_space.get_excluded_actions("obs")
         get_actions_return = fake_action_space.get_action(0, "obs")
 
+        # THEN
         assert isinstance(ActionSpaceBase, ABCMeta)
         assert is_action_done_return is None
         assert reset_return is None
@@ -26,11 +31,23 @@ class TestActionSpaceBase(unittest.TestCase):
         assert get_excluded_actions_return is None
 
     def test_get_named_actions(self):
+
+        # GIVEN
         fake_action_space = FakeActionSpace()
+
+        # WHEN
         fake_action_space.get_named_actions = MagicMock(return_value=[])
+
+        # THEN
         self.assertEqual(fake_action_space.get_named_actions(), [])
 
     def test_size(self):
+
+        # GIVEN
         fake_action_space = FakeActionSpace()
+
+        # WHEN
         fake_action_space.get_actions = MagicMock(return_value=[])
+
+        # THEN
         self.assertEqual(fake_action_space.size, 0)
--- tests/units/actions/test_chain_of_actions_base.py
+++ tests/units/actions/test_chain_of_actions_base.py
@@ -14,9 +14,14 @@ class TestChainOfActionsBase(unittest.TestCase):
 
 	def test_abstract_methods(self):
 
+		# GIVEN
 		fake_chain_of_actions = FakeChainOfActions()
+
+		# WHEN
 		get_action_return = fake_chain_of_actions.get_action(0)
 		length_return = fake_chain_of_actions.length
+
+		# THEN
 		assert isinstance(ChainOfActionsBase, ABCMeta)
 		assert get_action_return is None
 		assert (length_return == 0) is True
