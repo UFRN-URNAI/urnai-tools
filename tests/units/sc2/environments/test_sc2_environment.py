f98f34b7259b744068d6a830f6d824ebc759f05d
Henrique Lopes Fouquet <102550763+RickFqt@users.noreply.github.com>
2024-06-21 09:14:10 -0300

    Resolves #87 (#90)
    * feat(issue-87): Added support for two agents on environment
    
    * feat: Added support for multiple agents on sc2env
    
    * style: Removes comments

--- tests/units/sc2/environments/test_sc2_environment.py
+++ tests/units/sc2/environments/test_sc2_environment.py
@@ -16,10 +16,7 @@ class TestSC2Environment(unittest.TestCase):
     def test_start_on_init(self, MockSC2Env, MockAgentInterfaceFormat, MockDimensions):
         # GIVEN any state
         # WHEN
-        SC2Env(players=[
-                sc2_env.Agent(sc2_env.Race.protoss),
-                sc2_env.Bot(sc2_env.Race.terran, sc2_env.Difficulty.very_easy),
-            ])
+        SC2Env()
         # THEN
         MockSC2Env.assert_called_once()
         MockAgentInterfaceFormat.assert_called_once()
@@ -35,10 +32,7 @@ class TestSC2Environment(unittest.TestCase):
         # should only be instanciated once.
 
         # GIVEN
-        env = SC2Env(players=[
-                sc2_env.Agent(sc2_env.Race.terran),
-                sc2_env.Bot(sc2_env.Race.zerg, sc2_env.Difficulty.easy),
-            ])
+        env = SC2Env()
         # WHEN
         env.start()
         # THEN
@@ -64,7 +58,7 @@ class TestSC2Environment(unittest.TestCase):
         obs = env.reset()
         # THEN
         env.env_instance.reset.assert_called_once()
-        assert isinstance(obs, NamedDict)
+        assert isinstance(obs[0], NamedDict)
     
     @patch('pysc2.env.sc2_env.SC2Env')
     def test_step_method(self, scenv_mock):
@@ -84,12 +78,12 @@ class TestSC2Environment(unittest.TestCase):
         obs, reward, terminated, truncated = env.step([actions.RAW_FUNCTIONS.no_op()])
         # THEN
         env.env_instance.step.assert_called_once_with([actions.RAW_FUNCTIONS.no_op()])
-        assert isinstance(obs, NamedDict)
-        assert obs.step_mul == 16
-        assert isinstance(obs.map_size, point.Point)
-        assert obs.map_size.x == 64
-        assert obs.map_size.y == 64
-        assert reward == 0
+        assert isinstance(obs[0], NamedDict)
+        assert obs[0].step_mul == 16
+        assert isinstance(obs[0].map_size, point.Point)
+        assert obs[0].map_size.x == 64
+        assert obs[0].map_size.y == 64
+        assert reward[0] == 0
         assert terminated is False
         assert truncated is False
 
@@ -140,11 +134,55 @@ class TestSC2Environment(unittest.TestCase):
         # WHEN
         obs, reward, terminated, truncated = env._parse_timestep(fakeTimestep)
         # THEN
-        assert isinstance(obs, NamedDict)
-        assert obs.step_mul == 16
-        assert isinstance(obs.map_size, point.Point)
-        assert obs.map_size.x == 64
-        assert obs.map_size.y == 64
-        assert reward == 0
+        assert isinstance(obs[0], NamedDict)
+        assert obs[0].step_mul == 16
+        assert isinstance(obs[0].map_size, point.Point)
+        assert obs[0].map_size.x == 64
+        assert obs[0].map_size.y == 64
+        assert reward[0] == 0
+        assert terminated is False
+        assert truncated is False
+
+    @patch('pysc2.env.sc2_env.SC2Env')
+    def test_parse_timestep_multiple_agents(self, scenv_mock):
+
+        # GIVEN
+        env = SC2Env(map_name='Simple64', step_mul=16, game_steps_per_episode=100,
+                     players=[
+                        sc2_env.Agent(sc2_env.Race.protoss),
+                        sc2_env.Agent(sc2_env.Race.protoss)
+                    ])
+        fakeTimesteps = ( TimeStep( 
+            step_type= StepType.FIRST, 
+            reward=2, 
+            discount=0, 
+            observation=NamedDict({})),
+            TimeStep( 
+            step_type= StepType.FIRST, 
+            reward=4, 
+            discount=0, 
+            observation=NamedDict({}))
+        )
+        env.env_instance._episode_steps = 16
+        env.env_instance._episode_length = 100
+        env.env_instance._interface_formats[0]._raw_resolution = point.Point(64,64)
+        # WHEN
+        obs, reward, terminated, truncated = env._parse_timestep(fakeTimesteps)
+        # THEN
+        assert env.num_agents == 2
+        assert len(obs) == 2
+        assert isinstance(obs[0], NamedDict)
+        assert isinstance(obs[1], NamedDict)
+        assert obs[0].step_mul == 16
+        assert obs[1].step_mul == 16
+        assert isinstance(obs[0].map_size, point.Point)
+        assert isinstance(obs[1].map_size, point.Point)
+        assert obs[0].map_size.x == 64
+        assert obs[0].map_size.y == 64
+        assert obs[1].map_size.x == 64
+        assert obs[1].map_size.y == 64
+        assert len(reward) == 2
+        assert reward[0] == 2
+        assert reward[1] == 4
         assert terminated is False
         assert truncated is False
