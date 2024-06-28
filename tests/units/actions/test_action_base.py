de658706d4df397ed1ee8faaa6729c0d881f2925
Carlos Eduardo <carlosvallerosafilho@gmail.com>
2024-06-28 14:52:06 -0300

    Resolves #68 (#89)
    * test(issue-68): Add unit tests for ActionSpaceBase
    
    Also modified ActionBase's tests to test action name.
    
    * refactor(issue-68): Remove name attribute from Action
    
    #89
    
    * refactor(issue-68): Tests now in "given when then" format
    
    #89

--- tests/units/actions/test_action_base.py
+++ tests/units/actions/test_action_base.py
@@ -13,13 +13,17 @@ class TestActionBase(unittest.TestCase):
 
     def test_abstract_methods(self):
 
+        # GIVEN
         fake_action = FakeAction()
+
+        # WHEN
         run_return = fake_action.run()
         check_return = fake_action.check("observation")
-        is_complete_return = fake_action.is_complete()
+        is_complete_return = fake_action.is_complete
+
+        # THEN
         assert fake_action.__id__ is None
         assert isinstance(ActionBase, ABCMeta)
         assert run_return is None
         assert check_return is None
         assert is_complete_return is None
-
---
+++ urnai/actions/action_space_base.py
@@ -0,0 +1,61 @@
+from abc import ABC, abstractmethod
+
+
+class ActionSpaceBase(ABC):
+    """
+    ActionSpace works as an extra abstraction layer used by the agent
+    to select actions. This means the agent doesn't select actions from
+    action_set, but from ActionSpace.
+    
+    This class is responsible of telling the agent which actions it can 
+    use and which ones are excluded from selection. It can also force the
+    agent to use certain actions by combining them into multiple steps.
+    """
+
+    @abstractmethod
+    def is_action_done(self) -> bool:
+        """
+        Some agents must do multiple steps for a single action before they
+        can choose another one. This method should implement the logic to
+        tell whether the current action is done or not. That is, if all the 
+        steps for an action are complete.
+        """
+        ...
+
+    @abstractmethod
+    def reset(self) -> None:
+        """
+        Contains logic for resetting the action_space. This is used mostly
+        for agents that require multiple steps for a single action.
+        """
+        ...
+
+    @abstractmethod
+    def get_actions(self) -> list[int]:
+        """Returns all the actions that the agent can choose from."""
+        ...
+
+    @abstractmethod
+    def get_excluded_actions(self, obs) -> list[int]:
+        """Returns a subset of actions that can't be chosen by the agent."""
+        ...
+
+    @abstractmethod
+    def get_action(self, action_idx: int, obs):
+        """
+        Receives an action index as a parameter and returns the corresponding
+        action from the available actions. This method should return an action
+        that can be used by the environment's step method.
+        """
+        pass
+
+    def get_actions_id(self) -> list[str]:
+        """Returns the names of all the actions that the agent can choose from."""
+        ids = []
+        for action in self.get_actions():
+            ids.append(action.__id__)
+        return ids
+
+    @property
+    def size(self) -> int:
+        return len(self.get_actions())
