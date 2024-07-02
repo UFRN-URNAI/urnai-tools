b1d21e4a940ae7420b0bb499622af6d9195af04b
RickFqt <henriquefouquet@gmail.com>
2024-07-02 14:39:45 -0300

    feat: Added protoss and zerg states

---
+++ urnai/sc2/states/protoss_state.py
@@ -0,0 +1,78 @@
+import numpy as np
+from pysc2.env import sc2_env
+from pysc2.lib import units
+
+from urnai.sc2.states.states_utils import (
+    append_player_and_enemy_grids,
+    get_my_raw_units_amount,
+)
+from urnai.states.state_base import StateBase
+
+
+class ProtossState(StateBase):
+
+    def __init__(
+        self,
+        grid_size: int = 4,
+        use_raw_units: bool = True,
+        raw_resolution: int = 64,
+    ):
+        self.grid_size = grid_size
+        self.player_race = sc2_env.Race.protoss
+        self.use_raw_units = use_raw_units
+        self.raw_resolution = raw_resolution
+        self.reset()
+
+    def update(self, obs):
+        new_state = [
+            # Adds general information from the player.
+            obs.player.minerals / 6000,
+            obs.player.vespene / 6000,
+            obs.player.food_cap / 200,
+            obs.player.food_used / 200,
+            obs.player.food_army / 200,
+            obs.player.food_workers / 200,
+            (obs.player.food_cap - obs.player.food_used) / 200,
+            obs.player.army_count / 200,
+            obs.player.idle_worker_count / 200,
+        ]
+
+        if self.use_raw_units:
+            new_state.extend(
+                [
+                    # Adds information related to player's Protoss units/buildings.
+                    get_my_raw_units_amount(obs, units.Protoss.Nexus),
+                    get_my_raw_units_amount(obs, units.Protoss.Pylon),
+                    get_my_raw_units_amount(obs, units.Protoss.Assimilator),
+                    get_my_raw_units_amount(obs, units.Protoss.Forge),
+                    get_my_raw_units_amount(obs, units.Protoss.Gateway),
+                    get_my_raw_units_amount(obs, units.Protoss.CyberneticsCore),
+                    get_my_raw_units_amount(obs, units.Protoss.PhotonCannon),
+                    get_my_raw_units_amount(obs, units.Protoss.RoboticsFacility),
+                    get_my_raw_units_amount(obs, units.Protoss.Stargate),
+                    get_my_raw_units_amount(obs, units.Protoss.TwilightCouncil),
+                    get_my_raw_units_amount(obs, units.Protoss.RoboticsBay),
+                    get_my_raw_units_amount(obs, units.Protoss.TemplarArchive),
+                    get_my_raw_units_amount(obs, units.Protoss.DarkShrine),
+                ]
+            )
+            new_state = append_player_and_enemy_grids(
+                obs, new_state, self.grid_size, self.raw_resolution
+            )
+
+        self._dimension = len(new_state)
+        final_state = np.expand_dims(new_state, axis=0)
+        self._state = final_state
+        return final_state
+
+    @property
+    def state(self):
+        return self._state
+
+    @property
+    def dimension(self):
+        return self._dimension
+
+    def reset(self):
+        self._state = None
+        self._dimension = None
---
+++ urnai/sc2/states/zerg_state.py
@@ -0,0 +1,78 @@
+import numpy as np
+from pysc2.env import sc2_env
+from pysc2.lib import units
+
+from urnai.sc2.states.states_utils import (
+    append_player_and_enemy_grids,
+    get_my_raw_units_amount,
+)
+from urnai.states.state_base import StateBase
+
+
+class ZergState(StateBase):
+
+    def __init__(
+        self,
+        grid_size: int = 4,
+        use_raw_units: bool = True,
+        raw_resolution: int = 64,
+    ):
+        self.grid_size = grid_size
+        self.player_race = sc2_env.Race.zerg
+        self.use_raw_units = use_raw_units
+        self.raw_resolution = raw_resolution
+        self.reset()
+
+    def update(self, obs):
+        new_state = [
+            # Adds general information from the player.
+            obs.player.minerals / 6000,
+            obs.player.vespene / 6000,
+            obs.player.food_cap / 200,
+            obs.player.food_used / 200,
+            obs.player.food_army / 200,
+            obs.player.food_workers / 200,
+            (obs.player.food_cap - obs.player.food_used) / 200,
+            obs.player.army_count / 200,
+            obs.player.idle_worker_count / 200,
+        ]
+
+        if self.use_raw_units:
+            new_state.extend(
+                [
+                    # Adds information related to player's Zerg units/buildings.
+                    get_my_raw_units_amount(obs, units.Zerg.BanelingNest),
+                    get_my_raw_units_amount(obs, units.Zerg.EvolutionChamber),
+                    get_my_raw_units_amount(obs, units.Zerg.Extractor),
+                    get_my_raw_units_amount(obs, units.Zerg.Hatchery),
+                    get_my_raw_units_amount(obs, units.Zerg.HydraliskDen),
+                    get_my_raw_units_amount(obs, units.Zerg.InfestationPit),
+                    get_my_raw_units_amount(obs, units.Zerg.LurkerDen),
+                    get_my_raw_units_amount(obs, units.Zerg.NydusNetwork),
+                    get_my_raw_units_amount(obs, units.Zerg.RoachWarren),
+                    get_my_raw_units_amount(obs, units.Zerg.SpawningPool),
+                    get_my_raw_units_amount(obs, units.Zerg.SpineCrawler),
+                    get_my_raw_units_amount(obs, units.Zerg.Spire),
+                    get_my_raw_units_amount(obs, units.Zerg.SporeCrawler),
+                ]
+            )
+            new_state = append_player_and_enemy_grids(
+                obs, new_state, self.grid_size, self.raw_resolution
+            )
+
+        self._dimension = len(new_state)
+        final_state = np.expand_dims(new_state, axis=0)
+        self._state = final_state
+        return final_state
+
+    @property
+    def state(self):
+        return self._state
+
+    @property
+    def dimension(self):
+        return self._dimension
+
+    def reset(self):
+        self._state = None
+        self._dimension = None
