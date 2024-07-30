bc09fceb761e4afd7d9cb6ac369f2b728fbfc56d
CinquilCinquil <106356391+CinquilCinquil@users.noreply.github.com>
2024-07-30 09:32:18 -0300

    test(issue-98): Add units tests to SC2Action

---
+++ tests/units/sc2/actions/test_sc2_action.py
@@ -0,0 +1,21 @@
+import unittest
+
+from pysc2.lib import actions
+
+from urnai.actions.sc2_action import SC2Action
+
+_BUILD_REFINERY = actions.RAW_FUNCTIONS.Build_Refinery_pt
+_NO_OP = actions.FUNCTIONS.no_op
+
+class TestActionBase(unittest.TestCase):
+
+    def test_run(self):
+
+        run_no_op = SC2Action.run(_NO_OP)
+        run_build_refinery = SC2Action.run(_BUILD_REFINERY, 'now', 0)
+        
+        self.assertEqual(run_no_op.function, _NO_OP.id)
+        self.assertEqual(run_no_op.arguments, [])
+
+        self.assertEqual(run_build_refinery.function, _BUILD_REFINERY.id)
+        self.assertEqual(run_build_refinery.arguments, [[actions.Queued['now']], [0]])
