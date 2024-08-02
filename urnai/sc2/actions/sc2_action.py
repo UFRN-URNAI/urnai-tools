9332238bdcf5952bbe7e9a5c8538f33653cc015e
CinquilCinquil <106356391+CinquilCinquil@users.noreply.github.com>
2024-08-02 14:08:46 -0300

    refactor(issue-98): Rename action_id to action
    #98

--- urnai/sc2/actions/sc2_action.py
+++ urnai/sc2/actions/sc2_action.py
@@ -5,5 +5,5 @@ class SC2Action:
 
     """This class encapsulates the usage of actions from pysc2"""
 
-    def run(action_id, *args) -> FunctionCall:
-        return action_id(*args)
+    def run(action, *args) -> FunctionCall:
+        return action(*args)
