2091985b82fe6be7b04d2d1884f06ff06bba41d8
Carlos Eduardo <carlosvallerosafilho@gmail.com>
2024-08-02 16:29:20 -0300

    refactor(issue-98): Rename action to action_function
    #99
    
    Co-authored-by: Álvaro Paiva <alvarofepipa@gmail.com>

--- urnai/sc2/actions/sc2_action.py
+++ urnai/sc2/actions/sc2_action.py
@@ -5,5 +5,5 @@ class SC2Action:
 
     """This class encapsulates the usage of actions from pysc2"""
 
-    def run(action, *args) -> FunctionCall:
-        return action(*args)
+    def run(action_function, *args) -> FunctionCall:
+        return action_function(*args)
