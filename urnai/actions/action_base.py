51b5095dc6d0b21fdc9ff2685bb8ed172447213a
CinquilCinquil <106356391+CinquilCinquil@users.noreply.github.com>
2024-10-29 14:29:31 -0300

    feat: Split part of ActionBase into ActionBaseStrict

--- urnai/actions/action_base.py
+++ urnai/actions/action_base.py
@@ -1,21 +1,11 @@
 from abc import ABC, abstractmethod
+from typing import Any
 
 
 class ActionBase(ABC):
 	__id__ = None
 	
 	@abstractmethod
-	def run(self) -> None:
+	def run(*args) -> Any:
 		"""Executing the action"""
-		...
-	
-	@abstractmethod
-	def check(self, obs) -> bool:
-		"""Returns whether the action can be executed or not"""
-		...
-	
-	@property
-	@abstractmethod
-	def is_complete(self) -> bool:
-		"""Returns whether the action has finished or not"""
 		...
---
+++ urnai/actions/action_base_strict.py
@@ -0,0 +1,21 @@
+from abc import abstractmethod
+from typing import Any
+from urnai.actions.action_base import ActionBase
+
+class ActionBaseStrict(ActionBase):
+	
+	@abstractmethod
+	def run(*args) -> Any:
+		"""Executing the action"""
+		...
+
+	@abstractmethod
+	def check(self, obs) -> bool:
+		"""Returns whether the action can be executed or not"""
+		...
+	
+	@property
+	@abstractmethod
+	def is_complete(self) -> bool:
+		"""Returns whether the action has finished or not"""
+		...
