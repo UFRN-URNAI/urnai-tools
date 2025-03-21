d4016edb58c61602fb79bb87d0dcb47ee94dc3fc
Carlos Eduardo Valle <carlosvallerosafilho@gmail.com>
2025-03-21 17:29:18 -0300

    Resolves #108 (#117)
    test(issue-108): Include case where action_list has elements

--- tests/units/actions/test_chain_of_actions_base.py
+++ tests/units/actions/test_chain_of_actions_base.py
@@ -1,8 +1,15 @@
 import unittest
 from abc import ABCMeta
 
+from urnai.actions.action_base import ActionBase
 from urnai.actions.chain_of_actions_base import ChainOfActionsBase
 
+
+class FakeActionBase(ActionBase):
+	ActionBase.__abstractmethods__ = set()
+	
+	def __init__(self):
+		super().__init__()
 
 class FakeChainOfActions(ChainOfActionsBase):
 	ChainOfActionsBase.__abstractmethods__ = set()
@@ -18,10 +25,16 @@ class TestChainOfActionsBase(unittest.TestCase):
 		fake_chain_of_actions = FakeChainOfActions()
 
 		# WHEN
-		get_action_return = fake_chain_of_actions.get_action(0)
+		get_action_return_no_actions = fake_chain_of_actions.get_action(0)
+
+		fake_action = FakeActionBase()
+		fake_chain_of_actions.action_list.append(fake_action)
+		get_action_return_has_actions = fake_chain_of_actions.get_action(0)
+
 		length_return = fake_chain_of_actions.length
 
 		# THEN
 		assert isinstance(ChainOfActionsBase, ABCMeta)
-		assert get_action_return is None
-		assert (length_return == 0) is True
+		assert get_action_return_no_actions is None
+		self.assertEqual(get_action_return_has_actions, fake_action)
+		assert (length_return == 1) is True
