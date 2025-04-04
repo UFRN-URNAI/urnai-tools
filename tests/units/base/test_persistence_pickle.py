a5566073aa6170f2e6d4129db287aabc8f72e35d
Álvaro Paiva <alvarofepipa@gmail.com>
2025-04-04 18:28:11 -0300

    Resolve #111 Add test for PersistencePickle._simple_save (#114)
    * build: Upgrades mambaorg/micromamba from 1.4.9 to 2.0.2
    
    * fix: Downgrade Python from 3.11 to 3.10 to fix pysc2 incompatibility
    
    * fix(issue-111): Fix test for PersistencePickle._simple_save
    
    * chore: Decrease fail_under from 100.0 to 90.0
    
    * chore: Updates pre-push hook
    
    * build: Upgrades micromamba image from 2.0.2 to 2.0.8
    
    * refactor: Taskfile tasks
    
    * chore: Decrease fail_under from 90.0 to 70.0

--- tests/units/base/test_persistence_pickle.py
+++ tests/units/base/test_persistence_pickle.py
@@ -1,28 +1,26 @@
 import unittest
-from unittest.mock import patch
+from unittest.mock import mock_open, patch
 
 from urnai.base.persistence_pickle import PersistencePickle
 
 
-class FakePersistencePickle(PersistencePickle):
-    def __init__(self, threaded_saving=False):
-        super().__init__(threaded_saving)
-
 class TestPersistence(unittest.TestCase):
-
-    @patch('urnai.base.persistence_pickle.PersistencePickle._simple_save')
-    def test_simple_save(self, mock_simple_save):
-
+    @patch('urnai.base.persistence_pickle.os.makedirs')
+    @patch('urnai.base.persistence_pickle.open', mock_open(read_data=""))
+    @patch('urnai.base.persistence_pickle.pickle.dump')
+    def test_simple_save(self, mock_pickle_dump, mock_makedirs):
         # GIVEN
-        fake_persistence_pickle = FakePersistencePickle()
+        persistence_pickle = PersistencePickle()
         persist_path = "test_simple_save"
+        mock_makedirs.return_value = ""
+        mock_pickle_dump.return_value = ""
 
         # WHEN
-        mock_simple_save.return_value = "return_value"
-        simple_save_return = fake_persistence_pickle._simple_save(persist_path)
+        persistence_pickle._simple_save(persist_path)
 
         # THEN
-        self.assertEqual(simple_save_return, "return_value")
+        mock_makedirs.assert_called_once_with(persist_path, exist_ok=True)
+        self.assertEqual(mock_pickle_dump.call_count, 2)
 
     @patch('urnai.base.persistence_pickle.PersistencePickle.load')
     def test_load(self, mock_load):
@@ -31,36 +29,33 @@ class TestPersistence(unittest.TestCase):
         and saves it (state1). After that, it changes the object's
         attributes (state2) and loads it back to state1.
         """
-
         # GIVEN
-        fake_persistence_pickle = FakePersistencePickle()
+        persistence_pickle = PersistencePickle()
         persist_path = "test_load"
         mock_load.return_value = "return_value"
 
         # WHEN
-        load_return = fake_persistence_pickle.load(persist_path)
+        load_return = persistence_pickle.load(persist_path)
 
         # THEN
         self.assertEqual(load_return, "return_value")
 
     def test_get_attributes(self):
-
         # GIVEN
-        fake_persistence_pickle = FakePersistencePickle()
+        persistence_pickle = PersistencePickle()
 
         # WHEN
-        return_list = fake_persistence_pickle._get_attributes()
+        return_list = persistence_pickle._get_attributes()
 
         # THEN
         self.assertEqual(return_list, ['threaded_saving'])
 
     def test_get_dict(self):
-
         # GIVEN
-        fake_persistence_pickle = FakePersistencePickle()
+        persistence_pickle = PersistencePickle()
 
         # WHEN
-        return_dict = fake_persistence_pickle._get_dict()
+        return_dict = persistence_pickle._get_dict()
 
         # THEN
         self.assertEqual(return_dict, {"threaded_saving": False})
